745ee80aecb83c54eb0ecbe2001aa59d348545bb
Leon Magee <leonmagee33@gmail.com>
2018-08-13 20:49:59 -0700

    update

---
+++ database/migrations/2018_08_14_034644_report_type_site_values.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ReportTypeSiteValues extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('report_type_site_values', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('report_type_id');
+            $table->integer('site_id');
+            $table->integer('spiff_value');
+            $table->integer('residual_percent');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('settings');
+    }
+}
