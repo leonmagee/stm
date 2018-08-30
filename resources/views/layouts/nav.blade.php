3897560827c19acbb930b28b87f07b8c8c026b1f
Leon Magee <leonmagee33@gmail.com>
2018-08-30 11:03:50 -0700

    update

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,9 +1,28 @@
 <ul class="sidebar-menu">
 
+{{-- 	.flaticon-memory-card:before { content: "\f100"; } good
+.flaticon-sim-card:before { content: "\f101"; }
+.flaticon-sim-card-1:before { content: "\f102"; }
+.flaticon-sim:before { content: "\f103"; }
+.flaticon-growth:before { content: "\f104"; }
+.flaticon-report:before { content: "\f105"; }
+.flaticon-report-1:before { content: "\f106"; }
+.flaticon-user:before { content: "\f107"; }
+.flaticon-group:before { content: "\f108"; }
+.flaticon-user-1:before { content: "\f109"; }
+.flaticon-user-2:before { content: "\f10a"; }
+.flaticon-user-3:before { content: "\f10b"; }
+.flaticon-gear:before { content: "\f10c"; }
+.flaticon-gear-1:before { content: "\f10d"; }
+.flaticon-gear-2:before { content: "\f10e"; }
+.flaticon-upload-arrow:before { content: "\f10f"; }
+.flaticon-upload:before { content: "\f110"; }
+.flaticon-file-upload:before { content: "\f111"; } --}}
+
 	<li>
 
 		<div class="icon-wrap">
-			<i class="flaticon-memory-card"></i>
+			<i class="flaticon-sim-card"></i>
 		</div>
 
 		<a>Monthly Sims</a>
@@ -24,6 +43,10 @@
 
 	<li>
 
+		<div class="icon-wrap">
+			<i class="flaticon-report"></i>
+		</div>
+
 		<a>Report Types</a>
 
 		<ul class="sub-menu">
@@ -42,10 +65,22 @@
 
 	</li>
 
+	<li>
 
-	<li><a href="/sims/upload">Upload</a></li>
+		<div class="icon-wrap">
+			<i class="flaticon-upload"></i>
+		</div>
+
+		<a href="/sims/upload">Upload Sims</a>
+
+	</li>
 	
 	<li>
+
+		<div class="icon-wrap">
+			<i class="flaticon-group"></i>
+		</div>
+
 		<a>Users</a>
 	
 		<ul class="sub-menu">
@@ -60,6 +95,11 @@
 	
 	
 	<li>
+
+		<div class="icon-wrap">
+			<i class="flaticon-report-1"></i>
+		</div>
+
 		<a>User Sims</a>
 
 		<ul class="sub-menu">
@@ -77,6 +117,11 @@
 	</li>
 	
 	<li>
+
+		<div class="icon-wrap">
+			<i class="flaticon-gear"></i>
+		</div>
+
 		<a>Settings</a>
 
 		<ul class="sub-menu">
@@ -91,6 +136,14 @@
 
 	</li>
 	
-	<li><a href="/reports">Reports</a></li>
+	<li>
+
+		<div class="icon-wrap">
+			<i class="flaticon-growth"></i>
+		</div>
+
+		<a href="/reports">Reports</a>
+
+	</li>
 
 </ul>
