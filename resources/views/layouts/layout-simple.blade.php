8735832c872805a486b08e13a18d38fa69bcab46
Leon Magee <leonmagee33@gmail.com>
2018-09-10 08:38:13 -0700

    update

--- resources/views/layouts/layout-simple.blade.php
+++ resources/views/layouts/layout-simple.blade.php
@@ -1,12 +1,8 @@
 <!doctype html>
+
 <html lang="en">
-<head>
-	<meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<title>Sim Track Manager</title>
-	<link rel="stylesheet" href="/css/app.css">
-</head>
+
+@include('layouts.head')
 
 <body class='stm-body'>
 
--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -1,13 +1,8 @@
 <!doctype html>
+
 <html lang="en">
-<head>
-	<meta charset="utf-8" />
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<title>Sim Track Manager</title>
-	<link rel="stylesheet" href="/css/app.css">
-	<link rel="stylesheet" href="/fonts/vendor/my-icons-collection/font/flaticon.css">
-</head>
+
+@include('layouts.head')
 
 <body class='stm-body'>
 
