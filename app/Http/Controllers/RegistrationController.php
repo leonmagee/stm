4d26913bb4ecea8e5065ef8871b775ab07d8fab6
Leon Magee <leonmagee33@gmail.com>
2018-09-24 12:04:42 -0700

    update

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -80,7 +80,7 @@ class RegistrationController extends Controller
 		* Update this email to include branding and reflect that a user account was created.
 		* Mandate strong password... 
 		*/
-		\Mail::to($user)->send(new NewUser($user));
+		//\Mail::to($user)->send(new NewUser($user));
 
     	// log in new user
 		//auth()->login($user);
