990075c368a917124340129c60c8f86b4c099ddc
Leon Magee <leonmagee33@gmail.com>
2018-09-26 11:30:39 -0700

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -123,6 +123,27 @@ var options = {
 	legend: {
 		display: true,
 		position: 'right',
+		onClick: function(e, legendItem) {
+			  var index = legendItem.datasetIndex;
+			  var ci = this.chart;
+			  var alreadyHidden = (ci.getDatasetMeta(index).hidden === null) ? false : ci.getDatasetMeta(index).hidden;
+
+			  ci.data.datasets.forEach(function(e, i) {
+			    var meta = ci.getDatasetMeta(i);
+
+			    if (i !== index) {
+			      if (!alreadyHidden) {
+			        meta.hidden = meta.hidden === null ? !meta.hidden : null;
+			      } else if (meta.hidden === null) {
+			        meta.hidden = true;
+			      }
+			    } else if (i === index) {
+			      meta.hidden = null;
+			    }
+			  });
+
+			  ci.update();
+			}
 	}
 };
 
