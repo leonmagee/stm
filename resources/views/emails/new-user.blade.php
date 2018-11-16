9fa8d1b4528da047e2e162c41870815bdfd1cd11
Leon Magee <leonmagee33@gmail.com>
2018-11-16 09:21:43 -0800

    update

--- resources/views/emails/new-user.blade.php
+++ resources/views/emails/new-user.blade.php
@@ -1,7 +1,15 @@
 @component('mail::message')
 # Introduction
 
+# Hello {{ $user->name }}!
+
 You are now signed up to Sim Track Manager!
+
+Here is your login info:
+
+Username / Email: {{ $user->email }}
+
+Password: {{ $user->password }}
 
 @component('mail::button', ['url' => 'https://stmmax.com'])
 Go to Sim Track Manager
