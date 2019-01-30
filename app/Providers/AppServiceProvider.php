95a5deae407b09f12148b7d31b92056da75d94e7
Leon Magee <leonmagee33@gmail.com>
2019-01-30 10:10:21 -0800

    removed managers ability to upload sims

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -273,13 +273,6 @@ class AppServiceProvider extends ServiceProvider
                         'default' => false,
                     ],
                     [
-                        'name' => 'Upload Sims',
-                        'link' => '/sims/upload',
-                        'sub' => false,
-                        'icon' => 'flaticon-upload',
-                        'default' => false,
-                    ],
-                    [
                         'name' => 'All Users',
                         'link' => '/users',
                         'sub' => false,
--- routes/web.php
+++ routes/web.php
@@ -60,7 +60,7 @@ Route::group(['middleware' => 'App\Http\Middleware\LockOutUsersManagers'], funct
 /**
 * SIMs Routes
 */
-Route::group(['middleware' => 'App\Http\Middleware\LockOutUsersEmployees'], function()
+Route::group(['middleware' => 'App\Http\Middleware\LockOutUsersManagers'], function()
 {
 	Route::get('sims/upload', 'SimController@upload_form');
 });
