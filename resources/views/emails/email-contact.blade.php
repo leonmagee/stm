daa206f3fb14a83b9210fde8613af6418bff58ed
Leon Magee <leon@comic-con.org>
2019-10-15 20:53:19 -0700

    contact form update

--- resources/views/emails/email-contact.blade.php
+++ resources/views/emails/email-contact.blade.php
@@ -2,8 +2,16 @@
 
 # Hello {{ $admin->name }}!
 
-STM Contact form submitted by {{ $user->company }} - {{ $user->name }}
+STM Contact form submission.
 
+<strong style="font-size: 1.3em">{{ $user->company }}</strong><br />
+<strong>{{ $user->name }}</strong><br />
+<strong>{{ $user->address }}</strong><br />
+<strong>{{ $user->city }}, {{ $user->state }} {{ $user->zip }}</strong><br />
+<span style="color: #1b9aaa">{{ $user->email }}</span><br />
+<strong>{{ $user->phone }}</strong><br /><br />
+
+<strong>Message:</strong>
 {{ $message }}
 
 @component('mail::button', ['url' => 'https://stmmax.com'])
