b8a7e878b1ffb28dc23d9ccb65773efd25a3575a
Leon Magee <leon@comic-con.org>
2020-04-10 21:01:50 -0700

    update

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -2,12 +2,9 @@
 
   <div class="logo-wrap">
     <a href="/">
-      {{-- <img src="{{ URL::asset('img/stm_logo_white.png') }}" /> --}}
       <img src="{{ URL::asset('img/gs-stm-logo.png') }}" />
     </a>
-    {{-- @if (Auth::check()) --}}
     <a class="menu-toggle menu-modal-open"><i class="fas fa-bars"></i></a>
-    {{-- @endif --}}
   </div>
 
   <div class="header-area">
@@ -318,21 +315,21 @@
         </div>
         <div class="field has-addons">
           <p class="control">
-            <a class="button type not-link">
+            <a href="/redeem-credit" class="button user">
               <span class="icon is-small">
-                <i class="fas fa-sitemap"></i>
+                <i class="fi flaticon-security-on"></i>
               </span>
-              <span class="text">{{ $site }}</span>
+              <span class="text">Redeem Credit</span>
             </a>
           </p>
-          {{-- <p class="control">
-            <a class="button date not-link">
+          <p class="control">
+            <a href="/order-sims" class="button user">
               <span class="icon is-small">
-                <i class="fas fa-calendar"></i>
+                <i class="fi flaticon-sim"></i>
               </span>
-              <span class="text">{{ $current_date }}</span>
-          </a>
-          </p> --}}
+              <span class="text">Order Sims</span>
+            </a>
+          </p>
           <p class="control">
             <a href="/profile" class="button user">
               <span class="icon is-small">
@@ -373,81 +370,71 @@
       </div>
 
       <div class="mobile-menu">
-        {{-- <div class="field has-addons">
+        <div class="field has-addons">
           <p class="control">
-            <a class="button date not-link add-radius">
+            <a class="button logout" href="/logout">
               <span class="icon is-small">
-                <i class="fas fa-calendar"></i>
+                <i class="fas fa-times-circle"></i>
               </span>
-              <span class="text">{{ $current_date }}</span>
-        </a>
-        </p>
-      </div> --}}
-      <div class="field has-addons">
-        <p class="control">
-          <a class="button logout" href="/logout">
-            <span class="icon is-small">
-              <i class="fas fa-times-circle"></i>
-            </span>
-            <span class="text">Log Out</span>
-          </a>
-        </p>
-        <p class="control">
-          <a class="button mode not-link {{ $mode }}">
-            <span class="icon is-small">
-              @if($mode == 'online')
-              <i class="fas fa-signal"></i>
-              @else
-              <i class="fas fa-lock"></i>
-              @endif
-            </span>
-            <span class="text">{{ $mode }}</span>
-          </a>
-        </p>
+              <span class="text">Log Out</span>
+            </a>
+          </p>
+          <p class="control">
+            <a class="button mode not-link {{ $mode }}">
+              <span class="icon is-small">
+                @if($mode == 'online')
+                <i class="fas fa-signal"></i>
+                @else
+                <i class="fas fa-lock"></i>
+                @endif
+              </span>
+              <span class="text">{{ $mode }}</span>
+            </a>
+          </p>
+        </div>
+
       </div>
 
+      @endif
+
+      @else
+      {{-- menu not logged in --}}
+
+      <div class="large-menu-logged-out">
+        <li class="item login">
+          <a class="item-a" href="/">
+            <i class="fas fa-key"></i>
+            <span class="text">Login</span>
+          </a>
+        </li>
+        <li class="item about">
+          <a class="item-a" href="/about">
+            <i class="fas fa-users"></i>
+            <span class="text">About</span>
+          </a>
+        </li>
+        <li class="item contact">
+          <a class="item-a" href="/contact-us">
+            <i class="fas fa-envelope"></i>
+            <span class="text">Contact</span>
+          </a>
+        </li>
+        <li class="item links">
+          <a class="item-a">
+            <i class="fas fa-link"></i>
+            <span class="text">Links</span>
+          </a>
+          <div class="links-sub-menu" id="menu-direct-links">
+            <a href="https://h2odirectnow.com" target="_blank">H2O Direct</a>
+            <a href="https://pos.gswmax.com" target="_blank">Lyca Direct</a>
+            <a href="https://gsposa.instapayportal.com/login" target="_blank">GS Posa</a>
+          </div>
+        </li>
+
+      </div>
+
+      @endif
     </div>
-
-    @endif
-
-    @else
-    {{-- menu not logged in --}}
-
-    <div class="large-menu-logged-out">
-      <li class="item login">
-        <a class="item-a" href="/">
-          <i class="fas fa-key"></i>
-          <span class="text">Login</span>
-        </a>
-      </li>
-      <li class="item about">
-        <a class="item-a" href="/about">
-          <i class="fas fa-users"></i>
-          <span class="text">About</span>
-        </a>
-      </li>
-      <li class="item contact">
-        <a class="item-a" href="/contact-us">
-          <i class="fas fa-envelope"></i>
-          <span class="text">Contact</span>
-        </a>
-      </li>
-      <li class="item links">
-        <a class="item-a">
-          <i class="fas fa-link"></i>
-          <span class="text">Links</span>
-        </a>
-        <div class="links-sub-menu" id="menu-direct-links">
-          <a href="https://h2odirectnow.com" target="_blank">H2O Direct</a>
-          <a href="https://pos.gswmax.com" target="_blank">Lyca Direct</a>
-          <a href="https://gsposa.instapayportal.com/login" target="_blank">GS Posa</a>
-        </div>
-      </li>
-
-    </div>
-
-    @endif
   </div>
-</div>
 
 </div>
