6ea6de9bff27acdfc1eb0535e1d28d19a2a23af2
Leon Magee <leon@comic-con.org>
2020-04-28 14:32:24 -0700

    update

--- resources/views/registration/create.blade.php
+++ resources/views/registration/create.blade.php
@@ -28,7 +28,7 @@
           <div class="field">
             <label class="label" for="email">Email</label>
             <div class="control">
-              <input class="input" type="email" id="email" name="email" />
+              <input class="input" type="email" id="email" name="email" autocomplete="chrome-off" />
             </div>
           </div>
 
@@ -93,7 +93,7 @@
           <div class="field">
             <label class="label" for="password">Password</label>
             <div class="control">
-              <input class="input" type="password" id="password" name="password" />
+              <input class="input" type="password" id="password" name="password" autocomplete="chrome-off" />
             </div>
           </div>
 
