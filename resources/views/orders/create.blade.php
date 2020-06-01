e8d3e927eec9a344338df5e73f32623cda664092
Leon Magee <leon@comic-con.org>
2020-06-01 10:22:16 -0700

    update

--- resources/views/orders/create.blade.php
+++ resources/views/orders/create.blade.php
@@ -42,9 +42,31 @@
 
         <div class="field submit">
           <div class="control">
-            <button class="button is-primary call-loader" type="submit">Order Sims</button>
+            {{-- <button class="button is-primary call-loader" type="submit">Order Sims</button> --}}
+            <a class="button is-primary modal-open-email-blast">Order Sims</a>
           </div>
         </div>
+
+      </div>
+
+      <div class="modal" id="email-blast-modal">
+
+        <div class="modal-background"></div>
+
+        <div class="modal-content">
+
+          <div class="modal-box">
+
+            <h4 class="title">Are You Sure?</h4>
+
+            <button class="button is-danger call-loader" type="submit">Order Sims</button>
+
+            <a class="modal-email-close button is-primary">Cancel</a>
+          </div>
+
+        </div>
+
+        <a class="modal-close is-large" aria-label="close"></a>
 
       </div>
 
