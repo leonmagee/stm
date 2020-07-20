db69c81f82ea61d2c024c9904bdf8f9309d3c568
Leon Magee <leon@comic-con.org>
2020-07-19 18:30:36 -0700

    update

--- resources/views/purchases/show.blade.php
+++ resources/views/purchases/show.blade.php
@@ -3,35 +3,47 @@
 @section('content')
 
 <div class="form-wrapper">
-
   <div class="form-wrapper-inner">
-
-    <h3>Purchase | {{ $purchase->created_at->format('m/d/Y') }}</h3>
-
-    <div>
-      <label class="label">Company</label>
-      <span>{{ $purchase->user->company }}</span>
+    <h3>Purchase Record</h3>
+    <div class="purchase">
+      <div class="purchase__row purchase__row--header">
+        <div class="purchase_item">Purchase ID</div>
+        <div class="purchase_item">Company</div>
+        <div class="purchase_item">Name</div>
+        <div class="purchase_item">Total</div>
+        <div class="purchase_item">Date</div>
+      </div>
+      <div class="purchase__row purchase__row--body">
+        <div class="purchase_item">{{ $purchase->id }}</div>
+        <div class="purchase_item">{{ $purchase->user->company }}</div>
+        <div class="purchase_item">{{ $purchase->user->name }}</div>
+        <div class="purchase_item">${{ number_format($purchase->total, 2) }}</div>
+        <div class="purchase_item">{{ $purchase->created_at->format('m/d/Y') }}</div>
+      </div>
     </div>
-    <br />
-    <div>
-      <label class="label">User</label>
-      <span>{{ $purchase->user->name }}</span>
+    <div class="purchase">
+      <div class="purchase__row purchase__row--header">
+        <div class="purchase_item">Product ID</div>
+        <div class="purchase_item purchase_item--product_name">Product Name</div>
+        <div class="purchase_item">Color</div>
+        <div class="purchase_item">Quantity</div>
+        <div class="purchase_item">Unit Cost</div>
+        <div class="purchase_item">Discount</div>
+        <div class="purchase_item">Item Cost</div>
+      </div>
+      @foreach($purchase->products as $product)
+      <div class="purchase__row purchase__row--body">
+        <div class="purchase_item">{{ $product->product_id }}</div>
+        <div class="purchase_item purchase_item--product_name"><a
+            href="/products/{{ $product->product_id }}">{{ $product->name }}</a></div>
+        <div class=" purchase_item">{{ $product->variation }}</div>
+        <div class="purchase_item">{{ $product->quantity }}</div>
+        <div class="purchase_item">${{ number_format($product->price, 2) }}</div>
+        <div class="purchase_item">{{ $product->discount }}</div>
+        <div class="purchase_item">${{ number_format($product->price, 2) }}</div>
+      </div>
+      @endforeach
     </div>
-    <br />
-    <div>
-      <label class="label">Total</label>
-      <span>${{ number_format($purchase->total, 2) }}</span>
-    </div>
-    <br />
-    @foreach($purchase->products as $product)
-    <div>
-      <label class="label">Product</label>
-      <span>{{ $product->product_id }} - {{ $product->name }}</span>
-    </div>
-    <br />
-    @endforeach
-
   </div>
 </div>
-
 @endsection
