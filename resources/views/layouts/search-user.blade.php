1f37426cf05520a3d3d5f3d90ca6240b767e38a0
Leon Magee <leonmagee33@gmail.com>
2020-10-05 10:18:59 -0700

    update

--- resources/views/layouts/search-user.blade.php
+++ resources/views/layouts/search-user.blade.php
@@ -10,11 +10,9 @@
 <div class="field has-addons search-field">
   <form method="POST" action="/search-user" class="form-element" id="user-search-form">
     @csrf
-    <p class="control">
-      <span class="input-wrap">
-        <input type="text" class="search" name="user_search" placeholder="Search Users..." />
-      </span>
-    </p>
+    <span class="input-wrap">
+      <input type="text" class="search" name="user_search" placeholder="Search Users..." />
+    </span>
     <p class="control">
       <a class="button mode search-submit" id="user-search-submit">
         <span class="icon is-small">
