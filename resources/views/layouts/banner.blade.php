d38dadf9c4c71e7f272c10e257b2b08f0ebf746a
Leon Magee <leonmagee33@gmail.com>
2020-11-27 11:01:10 -0800

    update

--- resources/views/layouts/banner.blade.php
+++ resources/views/layouts/banner.blade.php
@@ -2,7 +2,7 @@
 @if($promotion)
 <div class="promo-banner">
   <div class="text">Limited time only - {{ $promotion->percent }}% off - use coupon code
-    <span>{{ $promotion->code }}</span></div>
+    <span>{{ $promotion->code }}</span> - sale end in</div>
   <div class="time" id="countdownTimer" secondsleft="{{ $promotion->seconds_left }}"></div>
 </div>
 @endif
