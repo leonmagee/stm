8199077cd554830b8e3e6fdcedecaba63aff839e
Leon Magee <leonmagee33@gmail.com>
2020-12-05 14:49:09 -0800

    update

---
+++ resources/assets/js/components/products/Heart.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+export default class Heart extends Component {
+    render() {
+        const { fav, toggle } = this.props;
+
+        let favClass = '';
+        if (fav) {
+            favClass = 'fav';
+        }
+        return (
+            <a
+                className={`product__footer--right product__footer--right-favorite ${favClass}`}
+                data-tooltip="Add To Favorites"
+                onClick={toggle}
+            >
+                <i className="fas fa-heart" />
+            </a>
+        );
+    }
+}
--- resources/assets/js/components/products/Product.js
+++ resources/assets/js/components/products/Product.js
@@ -2,112 +2,150 @@ import React, { Component } from 'react';
 import ReactStars from 'react-rating-stars-component';
 import Attributes from './Attributes';
 import Price from './Price';
+import Heart from './Heart';
 import ImageDiv from './ImageDiv';
 import Starz from './Starz';
 
 export default class Product extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      animate: false,
-      favorite: props.favorite,
+    constructor(props) {
+        super(props);
+        // const { favorite } = props;
+        // console.log(props);
+        // console.log('count', favorite);
+        this.state = {
+            animate: false,
+            // favorite,
+        };
     }
-  }
 
-  toggleFavorite() {
-
-    const favorite = !this.state.favorite;
-    axios({
-      method: "post",
-      url: "/toggle-favorite",
-      data: {
-        id: this.props.id
-      }
-    }).then(res => {
-      if(res.data === 'favorited') {
+    UNSAFE_componentWillReceiveProps(nextProps) {
         this.setState({
-            favorite: true
+            favorite: nextProps.favorite,
         });
-      } else if(res.data === 'un-favorited') {
+    }
+
+    //     componentWillReceiveProps(nextProps){
+    //     this.setState({ productdatail: nextProps.productdetailProps })
+    // }
+
+    toggleFavorite() {
+        // const favorite = !this.state.favorite;
+        const { id } = this.props;
+        // console.log('toggz', id);
+
+        axios({
+            method: 'post',
+            url: '/toggle-favorite',
+            data: {
+                id,
+            },
+        })
+            .then(res => {
+                if (res.data === 'favorited') {
+                    this.setState({
+                        favorite: true,
+                    });
+                } else if (res.data === 'un-favorited') {
+                    this.setState({
+                        favorite: false,
+                    });
+                }
+            })
+            .catch(err => {
+                console.log('error', err);
+            });
+    }
+
+    animateOff() {
         this.setState({
-            favorite: false
+            animate: false,
         });
-      }
-    }).catch(err => {
-      console.log('error', err);
-    });
-  }
-
-  animateOff() {
-    this.setState({
-      animate: false,
-    })
-  }
-
-  animateOn() {
-    this.setState({
-      animate: true,
-    })
-    setTimeout(function() {
-      this.animateOff();
-    }.bind(this)
-    , 1100);
-  }
-
-  addToCart(id) {
-    axios({
-      method: "post",
-      url: "/add-to-cart-axios",
-      data: {
-        id
-      }
-    }).then(res => {
-      //console.log('worked. res:', res);
-      this.animateOn();
-    }).catch(err => {
-      console.log('error', err);
-    });
-  }
-
-  render() {
-
-    const { id, img_url, discount, name, attributes, price, orig_price, rating, stock } = this.props;
-
-    const { animate, favorite } = this.state;
-
-    let animatePane = <div></div>;
-    if (animate) {
-      animatePane = <div className="product__cart_hover"><i className="fas fa-check"></i></div>;
     }
 
-    let addToCartButton = <div></div>;
-    if(stock) {
-                  addToCartButton = (
-                      <a
-                          className="product__footer--right product__footer--right-cart"
-                          data-tooltip="Add To Cart"
-                          onClick={() => this.addToCart(id)}
-                      >
-                          <i className="fas fa-cart-plus"></i>
-                      </a>
-                  );
-    } else {
-                  addToCartButton = (
-                      <a
-                          className="product__footer--right product__footer--right-cart"
-                          data-tooltip="Out of Stock"
-                      >
-                          <i className="fas fa-cart-plus"></i>
-                      </a>
-                  );
+    animateOn() {
+        this.setState({
+            animate: true,
+        });
+        setTimeout(
+            function() {
+                this.animateOff();
+            }.bind(this),
+            1100
+        );
     }
 
-    let favClass = favorite ? 'fav' : '';
+    addToCart(id) {
+        axios({
+            method: 'post',
+            url: '/add-to-cart-axios',
+            data: {
+                id,
+            },
+        })
+            .then(res => {
+                // console.log('worked. res:', res);
+                this.animateOn();
+            })
+            .catch(err => {
+                console.log('error', err);
+            });
+    }
+
+    render() {
+        const {
+            id,
+            img_url,
+            discount,
+            name,
+            attributes,
+            price,
+            orig_price,
+            rating,
+            stock,
+            // favorite,
+        } = this.props;
+
+        const { animate, favorite } = this.state;
+        // const { animate } = this.state;
+        // console.log(id, favorite);
+
+        let animatePane = <div />;
+        if (animate) {
+            animatePane = (
+                <div className="product__cart_hover">
+                    <i className="fas fa-check" />
+                </div>
+            );
+        }
+
+        let addToCartButton = <div />;
+        if (stock) {
+            addToCartButton = (
+                <a
+                    className="product__footer--right product__footer--right-cart"
+                    data-tooltip="Add To Cart"
+                    onClick={() => this.addToCart(id)}
+                >
+                    <i className="fas fa-cart-plus" />
+                </a>
+            );
+        } else {
+            addToCartButton = (
+                <a
+                    className="product__footer--right product__footer--right-cart"
+                    data-tooltip="Out of Stock"
+                >
+                    <i className="fas fa-cart-plus" />
+                </a>
+            );
+        }
+
+        // const favClass = favorite ? 'fav' : '';
+        // console.log(favClass, id, price);
         return (
             <div className="product">
                 {animatePane}
-                <a className="product__link" href={"/products/" + id}>
+                <a className="product__link" href={`/products/${id}`}>
                     <ImageDiv
                         img_url={img_url}
                         discount={discount}
@@ -125,16 +163,13 @@ export default class Product extends Component {
                         orig_price={orig_price}
                         discount={discount}
                     />
-                    <a
-                        className={'product__footer--right product__footer--right-favorite ' + favClass}
-                        data-tooltip="Add To Favorites"
-                        onClick={() => this.toggleFavorite()}
-                    >
-                        <i className="fas fa-heart"></i>
-                    </a>
+                    <Heart
+                        fav={favorite}
+                        toggle={() => this.toggleFavorite()}
+                    />
                     {addToCartButton}
                 </div>
             </div>
         );
-  }
+    }
 }
