3245c1104c0fb3ce3e78a9cd98d6029624a0c900
Leon Magee <leonmagee33@gmail.com>
2020-12-14 19:04:29 -0800

    update

--- resources/views/products/fav-saved-item.blade.php
+++ resources/views/products/fav-saved-item.blade.php
@@ -6,11 +6,11 @@
 <div class="compare__item compare__item--rating">
   <div class="rate_yo_wish_list" class="rate_yo_no_hover" rating="{{ (floor($item->get_rating() * 2) / 2) }}"></div>
 </div>
-<div class="compare__item">
+<div class="compare__item compare__item--action">
   @if($item->in_stock())
-  <a class="compare__item--add-to-cart" href="/add-to-cart-sav-fav/{{ $item->id }}">Add to Cart <i
+  <a class="compare__item--button compare__item--add-to-cart" href="/add-to-cart-sav-fav/{{ $item->id }}">Add to Cart <i
       class="fas fa-cart-plus"></i></a>
   @else
-  <span class="compare__item--sold-out">Sold Out</span>
+  <span class="compare__item--button compare__item--sold-out">Out of Stock</span>
   @endif
 </div>
