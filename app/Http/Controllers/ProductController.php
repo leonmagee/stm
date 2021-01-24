043d3c132c544d1d5359a26deec1a77a5a002db6
Leon Magee <leonmagee33@gmail.com>
2021-01-23 17:17:21 -0800

    update

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -826,10 +826,10 @@ class ProductController extends Controller
      */
     public static function img_replace($content)
     {
-        $pattern = '/<img .*alt="YES[^>]*>/i';
+        $pattern = '/<img alt="YES[^>]*>/i';
         $replacement = '<i class="fas fa-check"></i>';
         $updated = preg_replace($pattern, $replacement, $content);
-        $pattern = '/<img .*alt="NO[^>]*>/i';
+        $pattern = '/<img alt="NO[^>]*>/i';
         $replacement = '<i class="fas fa-times"></i>';
         return preg_replace($pattern, $replacement, $updated);
     }
